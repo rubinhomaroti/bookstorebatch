1ecf84ed749c66b37a606a894d6b461a45f8d1cf
Rubens Maroti F <rubens@mobilemed.com.br>
2021-08-25 09:54:25 -0300

    Initial commit

---
+++ src/main/java/br/com/rubinhomaroti/bookstorebatch/BookstorebatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.rubinhomaroti.bookstorebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorebatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rubinhomaroti/bookstorebatch/BookstorebatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rubinhomaroti.bookstorebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorebatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
